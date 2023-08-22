dac3691e7a987aa3ca44a8ddc16297d12cd71339
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2023-08-22 09:36:22 +0530

    update

--- Lectures/index.js
+++ Lectures/index.js
@@ -5,26 +5,25 @@ const PORT = 7700;
 
 app.get("/search", (req, res) => {
   const { q } = req.query;
-  if(!q){
+  if (!q) {
     res.send(`<h1>Nothing searched</h1>`);
+  } else {
+    res.status(200).json({ q });
   }
-  else{
-      res.status(200).json({q});
-  }
-});
-app.get("/searchanything", (req, res) => {
-  const q = req.query;
-      res.status(200).json({q});
 });
 
+app.get("/searchanything", (req, res) => {
+  const q = req.query;
+  res.status(200).json({ q });
+});
 
 app.get("/:username", (req, res) => {
   const username = req.params.username.toUpperCase();
-  res.status(200).json({username});
+  res.status(200).json({ username });
 });
 app.get("*", (req, res) => {
-    res.status(404).send("this path does not exist");
-  });
+  res.status(404).send("this path does not exist");
+});
 
 app.listen(PORT, () => {
   console.log(`app listening on http://localhost:${PORT}`);
